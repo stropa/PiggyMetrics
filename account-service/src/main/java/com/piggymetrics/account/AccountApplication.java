ac68bb3a094206c1f0ec5ff858f383e9316fe9f9
Maxim Konovalov <maxim.konovalov@onefactor.com>
2018-06-12 17:57:50 +0300

    simplifying

--- account-service/src/main/java/com/piggymetrics/account/AccountApplication.java
+++ account-service/src/main/java/com/piggymetrics/account/AccountApplication.java
@@ -1,11 +1,16 @@
 package com.piggymetrics.account;
 
+import com.google.common.collect.Maps;
 import com.piggymetrics.account.service.security.CustomUserInfoTokenServices;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigUtil;
 import feign.RequestInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
@@ -18,6 +23,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.*;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
@@ -27,15 +33,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
-import org.stropa.autodoc.engine.AutodocJavaEngine;
-import org.stropa.autodoc.engine.Item;
-import org.stropa.autodoc.reporters.DockerContainerDescriber;
-import org.stropa.autodoc.reporters.HostnameDescriber;
+import org.stropa.autodoc.org.stropa.autodoc.storage.FileStorage;
 import org.stropa.autodoc.spring.config.AutodocProperties;
+import org.stropa.autodoc.storage.ESStorage;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.stropa.autodoc.engine.AutodocJavaEngine.*;
 
 @SpringBootApplication
 @EnableResourceServer
@@ -46,9 +51,16 @@ import java.util.Map;
 @EnableConfigurationProperties
 @Configuration
 public class AccountApplication extends ResourceServerConfigurerAdapter implements ApplicationContextAware {
+	private static final Logger log = LoggerFactory.getLogger(AccountApplication.class);
 
 	@Autowired
 	private ResourceServerProperties sso;
+
+	@Autowired
+	private AutodocProperties autodocProperties;
+
+	@Value("${autodoc.storage.type}")
+	private String autodocStorageType;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
@@ -83,7 +95,7 @@ public class AccountApplication extends ResourceServerConfigurerAdapter implemen
 	}
 
 	@Bean
-	@ConfigurationProperties(prefix = "autodoc")
+	@ConfigurationProperties("")
 	AutodocProperties autodocProperties() {
 		return new AutodocProperties();
 	}
@@ -92,21 +104,60 @@ public class AccountApplication extends ResourceServerConfigurerAdapter implemen
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 
-		Map<String, String> autodoc = autodocProperties().autodoc();
-		Config autodocConfig;
-		if (autodoc.isEmpty()) {
-			autodocConfig = ConfigFactory.load();
-		} else {
-			autodocConfig = ConfigFactory.parseMap(autodoc);
+		System.out.println(autodocStorageType);
+
+		Map<String, Object> map = new HashMap();
+		MutablePropertySources propertySources = ((AbstractEnvironment) applicationContext.getEnvironment()).getPropertySources();
+		for(Iterator it = propertySources.iterator(); it.hasNext(); ) {
+			PropertySource propertySource = (PropertySource) it.next();
+			diveIntoPropertySource(map, propertySource, new ArrayList<>());
 		}
 
-		AutodocJavaEngine doc = new AutodocJavaEngine(autodocConfig);
+		System.out.println(map);
+		Map<String, Object> autodocConfigMap = Maps.filterKeys(map, key -> key != null && key.startsWith("autodoc"));
+		autodocConfigMap = autodocConfigMap.entrySet().stream().collect(
+				Collectors.toMap(e -> e.getKey().substring(("autodoc" + ".").length()), e -> e.getValue())
+		);
 
-		doc.addInfo(new HostnameDescriber().report(), Collections.emptyList());
+		//Map<String, String> autodoc = autodocProperties.autodoc();
+		Config autodocConfig;
+		if (autodocConfigMap.isEmpty()) {
+			System.out.println("Autodoc config is empty, loading defaults");
+			autodocConfig = ConfigFactory.load();
+		} else {
+			autodocConfig = ConfigFactory.parseMap(autodocConfigMap);
+			System.out.println("Got config for autodoc: " + autodocConfig.toString());
+		}
 
-		List<Item> items = new DockerContainerDescriber().report();
-		doc.addInfo(items, Collections.emptyList());
+		//AutodocJavaEngine docEngine = new AutodocJavaEngine(autodocConfig).ableToDoc("java", "spring", "docker", "networking");
 
-		doc.writeSnapshot();
+
+		doc("hostname");
+		doc("docker-container");
+		doc("application");
+
+
+		writeDocs(new FileStorage("autodoc.log"));
+	}
+
+
+
+
+
+
+
+	private void diveIntoPropertySource(Map<String, Object> map, PropertySource propertySource, List<PropertySource> seenAlready) {
+		if (propertySource instanceof MapPropertySource) {
+            map.putAll(((MapPropertySource) propertySource).getSource());
+        } else if (propertySource instanceof CompositePropertySource) {
+			for (PropertySource ps : ((CompositePropertySource) propertySource).getPropertySources()) {
+				if (seenAlready.contains(ps)) {
+					// skip
+				} else {
+					seenAlready.add(ps);
+					diveIntoPropertySource(map, ps, seenAlready);
+				}
+			}
+		}
 	}
 }
